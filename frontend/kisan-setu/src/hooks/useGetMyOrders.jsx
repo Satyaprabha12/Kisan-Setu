e6bfc4793fb216ea94aec2d71ad7ebc78a8248ed
Satyaprabha12 <satyaprabha747@gmail.com>
2025-11-16 23:02:33 +0530

    order hook changes

--- frontend/kisan-setu/src/hooks/useGetMyOrders.jsx
+++ frontend/kisan-setu/src/hooks/useGetMyOrders.jsx
@@ -9,6 +9,11 @@ function useGetMyOrders() {
     const dispatch=useDispatch()
     const {userData}=useSelector(state=>state.user)
   useEffect(()=>{
+    if (!userData) {
+            console.log("User not ready yet, skipping my-orders fetch.")
+            return;
+        }
+        
   const fetchOrders=async () => {
     try {
            const result=await axios.get(`${serverUrl}/api/order/my-orders`,{withCredentials:true})
