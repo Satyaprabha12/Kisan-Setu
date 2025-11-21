7a88abd0d78d9003a6cbeac6bf3e64a6573d53c1
Satyaprabha12 <satyaprabha747@gmail.com>
2025-11-22 01:24:32 +0530

    user dashboard updated

--- frontend/kisan-setu/src/components/userDashboard.jsx
+++ frontend/kisan-setu/src/components/userDashboard.jsx
@@ -1,82 +1,94 @@
-import banner1 from "../assets/user-banner.jpg"
-import banner2 from "../assets/user-banner2.jpg"
-import banner3 from "../assets/user-banner3.avif"
-import banner4 from "../assets/user-banner4.jpg"
-import { useEffect, useRef, useState } from "react"
-import Nav from "./nav"
-import { categories } from "../category"
-import CategoryCard from "./categoryCard"
-import { FaCircleChevronLeft } from "react-icons/fa6"
-import { FaCircleChevronRight } from "react-icons/fa6"
-import { useSelector } from "react-redux"
-import FoodCard from "./foodCard"
+import banner1 from "../assets/user-banner.jpg";
+import banner2 from "../assets/user-banner2.jpg";
+import banner3 from "../assets/user-banner3.avif";
+import banner4 from "../assets/user-banner4.jpg";
+import { useEffect, useRef, useState } from "react";
+import Nav from "./nav";
+import { categories, CATEGORY_MAP } from "../category";
+import CategoryCard from "./categoryCard";
+import { FaCircleChevronLeft } from "react-icons/fa6";
+import { FaCircleChevronRight } from "react-icons/fa6";
+import { useSelector } from "react-redux";
+import FoodCard from "./foodCard";
 import aboutImg from "../assets/aboutimg.jpg"
-import { AiTwotoneMail } from "react-icons/ai"
-import { FaPhoneVolume } from "react-icons/fa6"
-import { IoLocationSharp } from "react-icons/io5"
-import { FaFacebook } from "react-icons/fa6"
-import { FaTwitter } from "react-icons/fa"
-import { GrInstagram } from "react-icons/gr"
-import { BsLinkedin } from "react-icons/bs"
-import { FaArrowUp } from "react-icons/fa"
+import { AiTwotoneMail } from "react-icons/ai";
+import { FaPhoneVolume } from "react-icons/fa6";
+import { IoLocationSharp } from "react-icons/io5";
+import { FaFacebook } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa";
+import { GrInstagram } from "react-icons/gr";
+import { BsLinkedin } from "react-icons/bs";
+import { FaArrowUp } from "react-icons/fa";
 
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 function UserDashboard() {
   const { currentCity, shopInMyCity, itemsInMyCity, searchItems, userData } =
-    useSelector((state) => state.user)
-  const cateScrollRef = useRef()
-  const shopScrollRef = useRef()
-  const navigate = useNavigate()
-  const [showLeftCateButton, setShowLeftCateButton] = useState(false)
-  const [showRightCateButton, setShowRightCateButton] = useState(false)
-  const [showLeftShopButton, setShowLeftShopButton] = useState(false)
-  const [showRightShopButton, setShowRightShopButton] = useState(false)
-  const [updatedItemsList, setUpdatedItemsList] = useState([])
-  const banners = [banner1, banner2, banner3, banner4]
-  const [currentIndex, setCurrentIndex] = useState(0)
+    useSelector((state) => state.user);
+  const cateScrollRef = useRef();
+  const shopScrollRef = useRef();
+  const navigate = useNavigate();
+  const [showLeftCateButton, setShowLeftCateButton] = useState(false);
+  const [showRightCateButton, setShowRightCateButton] = useState(false);
+  const [showLeftShopButton, setShowLeftShopButton] = useState(false);
+  const [showRightShopButton, setShowRightShopButton] = useState(false);
+  const [updatedItemsList, setUpdatedItemsList] = useState([]);
+  const banners = [banner1, banner2, banner3, banner4];
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % banners.length)
-    }, 3000)
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % banners.length);
+    }, 3000);
 
-    return () => clearInterval(interval)
-  }, [banners.length])
+    return () => clearInterval(interval);
+  }, [banners.length]);
 
   const handleFilterByCategory = (category) => {
+
     if (category === "All") {
-      setUpdatedItemsList(itemsInMyCity)
-    } else {
-      const filteredList = itemsInMyCity?.filter(
-        (i) => i.category?.toLowerCase().includes(category.toLowerCase())
-      )
-      console.log("Filtered List:", filteredList)
-      setUpdatedItemsList(filteredList)
+      setUpdatedItemsList(itemsInMyCity);
+      return;
     }
-  }
+
+    const allowedNames = CATEGORY_MAP[category.toLowerCase()];
+
+    if (!allowedNames) {
+      setUpdatedItemsList(itemsInMyCity);
+      return;
+    }
+
+    const filtered = itemsInMyCity.filter(item =>
+      allowedNames.includes(item.name)
+    );
+
+    setUpdatedItemsList(filtered);
+
+
+  };
+
 
   useEffect(() => {
-    setUpdatedItemsList(itemsInMyCity)
-  }, [itemsInMyCity])
+    setUpdatedItemsList(itemsInMyCity);
+  }, [itemsInMyCity]);
 
   const updateButton = (ref, setLeftButton, setRightButton) => {
-    const element = ref.current
+    const element = ref.current;
     if (element) {
-      setLeftButton(element.scrollLeft > 0)
+      setLeftButton(element.scrollLeft > 0);
       setRightButton(
         element.scrollLeft + element.clientWidth < element.scrollWidth
-      )
+      );
     }
-  }
+  };
   const scrollHandler = (ref, direction) => {
     if (ref.current) {
       ref.current.scrollBy({
         left: direction == "left" ? -200 : 200,
         behavior: "smooth",
-      })
+      });
     }
-  }
+  };
 
   useEffect(() => {
     if (cateScrollRef.current) {
@@ -84,26 +96,26 @@ function UserDashboard() {
         cateScrollRef,
         setShowLeftCateButton,
         setShowRightCateButton
-      )
+      );
       updateButton(
         shopScrollRef,
         setShowLeftShopButton,
         setShowRightShopButton
-      )
+      );
       cateScrollRef.current.addEventListener("scroll", () => {
         updateButton(
           cateScrollRef,
           setShowLeftCateButton,
           setShowRightCateButton
-        )
-      })
+        );
+      });
       shopScrollRef.current.addEventListener("scroll", () => {
         updateButton(
           shopScrollRef,
           setShowLeftShopButton,
           setShowRightShopButton
-        )
-      })
+        );
+      });
     }
 
     return () => {
@@ -112,17 +124,17 @@ function UserDashboard() {
           cateScrollRef,
           setShowLeftCateButton,
           setShowRightCateButton
-        )
-      })
+        );
+      });
       shopScrollRef?.current?.removeEventListener("scroll", () => {
         updateButton(
           shopScrollRef,
           setShowLeftShopButton,
           setShowRightShopButton
-        )
-      })
-    }
-  }, [categories])
+        );
+      });
+    };
+  }, [categories]);
 
   return (
     <div className="w-screen min-h-screen flex flex-col gap-5 items-center overflow-y-auto">
@@ -198,7 +210,7 @@ function UserDashboard() {
 
       <div className="w-full max-w-6xl flex flex-col gap-5 items-start p-2.5">
         <h1 className="text-gray-800 text-2xl font-serif font-bold sm:text-3xl">
-          Best Farmers Shop in {currentCity}
+          Best Farmers shops in {currentCity}
         </h1>
         <div className="w-full relative">
           {showLeftShopButton && (
@@ -253,49 +265,50 @@ function UserDashboard() {
         </p>
       </section>
 
-      <footer className="w-full max-w-full p-5 mt-10 bg-black rounded-t-lg shadow-md flex flex-col md:flex-row justify-between items-center gap-6">
+      <footer className="w-full max-w-full p-5 mt-10 bg-black rounded-lg shadow-md flex flex-col md:flex-row justify-between items-center gap-6">
         <div>
-         <h2 className="text-4xl font-serif font-semibold mb-4 text-orange-400">Contact Us</h2>
-  <ul className="list-disc list-inside text-white/80 leading-relaxed space-y-2">
-    <li>
-      <span>
-        <AiTwotoneMail size={25} className="text-orange-500 inline mr-2" />
-        Email: support@kisansetuae.com
-      </span>
-    </li>
-    <li>
-      <FaPhoneVolume size={20} className="text-orange-500 inline mr-2"/>
-      <span>Phone: +91 9876543210</span>
-    </li>
-    <li>
-      <IoLocationSharp size={25} className="text-orange-500 inline mr-2" />
-      <span>Address: 123 Farm Road, Mumbai, Maharastra.</span>
-    </li>
-  </ul> 
+          <h2 className="text-4xl font-serif font-semibold mb-4 text-orange-400">Contact Us</h2>
+          <ul className="list-disc list-inside text-white/80 leading-relaxed space-y-2">
+            <li>
+              <span>
+                <AiTwotoneMail size={25} className="text-orange-500 inline mr-2" />
+                Email: support@kisansetuae.com
+              </span>
+            </li>
+            <li>
+              <FaPhoneVolume size={20} className="text-orange-500 inline mr-2" />
+              <span>Phone: +91 9876543210</span>
+            </li>
+            <li>
+              <IoLocationSharp size={25} className="text-orange-500 inline mr-2" />
+              <span>Address: 123 Farm Road, Mumbai, Maharastra.</span>
+            </li>
+          </ul>
         </div>
         <div className="flex flex-col items-center md:items-end space-y-3">
-        <button
-    onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-    className="flex items-center gap-2 px-4 py-2 bg-orange-500 hover:bg-orange-600 text-white rounded-full transition mb-5"
-  >
-    <FaArrowUp />
-    Back to Top
-  </button>
-  
+          <button
+            onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+            className="flex items-center gap-2 px-4 py-2 bg-orange-500 hover:bg-orange-600 text-white rounded-full transition mb-5"
+          >
+            <FaArrowUp />
+            Back to Top
+          </button>
 
-    <span className="text-2xl text-orange-500 font-semibold font-serif mb-3">Get Connect with Us</span>
-    <div className="flex gap-4 text-white cursor-pointer text-2xl">
-      <FaFacebook size={30} className="text-gray-300 hover:scale-110 hover:text-blue-600 transition" />
-      <FaTwitter size={30} className="text-gray-300 hover:scale-110 hover:text-blue-400 transition" />
-      <GrInstagram size={30}  className="text-gray-300 hover:scale-110 hover:text-pink-500 transition" />
-      <BsLinkedin size={30} className="text-gray-300 hover:scale-110 hover:text-blue-700 transition" />
-    </div>
-  </div>
-  
-</footer>
+
+          <span className="text-2xl text-orange-500 font-semibold font-serif mb-3">Get Connect with Us</span>
+          <div className="flex gap-4 text-white cursor-pointer text-2xl">
+
+            <FaFacebook size={30} className="text-gray-300 hover:scale-110 hover:text-blue-600 transition" />
+            <FaTwitter size={30} className="text-gray-300 hover:scale-110 hover:text-blue-400 transition" />
+            <GrInstagram size={30} className="text-gray-300 hover:scale-110 hover:text-pink-500 transition" />
+            <BsLinkedin size={30} className="text-gray-300 hover:scale-110 hover:text-blue-700 transition" />
+          </div>
+        </div>
+
+      </footer>
 
     </div>
-  )
+  );
 }
 
-export default UserDashboard
+export default UserDashboard;
